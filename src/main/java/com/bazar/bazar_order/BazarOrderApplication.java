0fe9741bc9481ed3defefa582f6147b5645e03b6
amrkhanfar <ultraakch@gmail.com>
2025-04-18 18:35:46 +0300

    Creating Bazar order service

---
+++ src/main/java/com/bazar/bazar_order/BazarOrderApplication.java
@@ -0,0 +1,13 @@
+package com.bazar.bazar_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarOrderApplication.class, args);
+	}
+
+}
