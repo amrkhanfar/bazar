6801606d6fa707ef2acf0701427687221000352c
amrkhanfar <ultraakch@gmail.com>
2025-04-18 22:23:08 +0300

    Creating Bazar frontend service

---
+++ src/main/java/com/bazar/bazar_frontend/BazarFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bazar.bazar_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarFrontendApplication.class, args);
+	}
+
+}
